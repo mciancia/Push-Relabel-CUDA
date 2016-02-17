7ee5c53ef5719786508968ca88669ddfa289962f
Mateusz Cianciara <e.cianciara@gmail.com>
2016-02-17 23:29:25 +0100

    cleanup

--- pushrelabel.cu
+++ pushrelabel.cu
@@ -5,21 +5,11 @@ extern "C" {
 __global__ void testOnGPU(int* capacityGPU, int* excessGPU, int* heightGPU, 
 	int N, int s_x, int s_y, int t_x, int t_y, int* changed, int *mapGPU)
 {
-	// *changed = 0;
-	// int blockId = blockIdx.x + blockIdx.y * gridDim.x; 
-	//int u = blockId * (blockDim.x * blockDim.y) + (threadIdx.y * blockDim.x) + threadIdx.x;
 	
 	int x = threadIdx.x + blockIdx.x*blockDim.x;
     int y = threadIdx.y + blockIdx.y*blockDim.y;
     int u = GID(x, y);
-    // testing macro and coordinatce calculations
-    // mapGPU[u] = x*100 + y;
-    // mapGPU[u] = GID(x, y);
-    // mapGPU[u] = ( u==GID(x, y) ? 1 : 0 );
-    // return;
-
-
-
+    
 	// exit if source or sink
 	if( (GID(s_x, s_y) == u) || (GID(t_x, t_y) == u)){
 		return;
@@ -29,7 +19,6 @@ __global__ void testOnGPU(int* capacityGPU, int* excessGPU, int* heightGPU,
 		int e_temp = excessGPU[u];
 		int h_temp = INF;
 		int v_temp = -1;
-		// int v_tempY = -1;
 		int u_to_v = -1; 
 		int v_to_u = -1;
 
@@ -80,53 +69,6 @@ __global__ void testOnGPU(int* capacityGPU, int* excessGPU, int* heightGPU,
 				h_temp = heightGPU[v];
 			}
 		}
-		// //EDGE UP
-		// if(y > 0){
-		// 	int v = GID(x, y-1);
-		// 	if(heightGPU[v] < h_temp && capacityGPU[4*u+0] > 0){
-		// 		v_temp = GID(x, y-1);
-		// 		// v_tempY = y-1;
-		// 		u_to_v = 0;
-		// 		v_to_u = 2;
-		// 		h_temp = heightGPU[v];
-		// 	}
-		// }
-
-		// //EDGE DOWN
-		// if(y < N-1){
-		// 	int v = GID(x, y+1);
-		// 	if(heightGPU[v] < h_temp && capacityGPU[4*u+2] > 0){
-		// 		v_temp = GID(x, y+1);
-		// 		// v_tempY = y+1;
-		// 		u_to_v = 2;
-		// 		v_to_u = 0;
-		// 		h_temp = heightGPU[v];
-		// 	}
-		// }
-
-		// //EDGE LEFT
-		// if(x > 0){
-		// 	int v = GID(x-1, y);
-		// 	if(heightGPU[v] < h_temp && capacityGPU[4*u+1] > 0){
-		// 		v_temp = GID(x-1, y);
-		// 		// v_tempY = y;
-		// 		u_to_v = 1;
-		// 		v_to_u = 3;
-		// 		h_temp = heightGPU[v];
-		// 	}
-		// }
-
-		// //EDGE RIGHT
-		// if(x < N-1){
-		// 	int v = GID(x+1, y);
-		// 	if(heightGPU[v] < h_temp && capacityGPU[4*u+3] > 0){
-		// 		v_temp = GID(x+1, y);
-		// 		// v_tempY = y;
-		// 		u_to_v = 3;
-		// 		v_to_u = 1;
-		// 		h_temp = heightGPU[v];
-		// 	}
-		// }
 
 		if(v_temp == -1 ){
 			printf("Something is wrong: %d %d\n", x, y);
